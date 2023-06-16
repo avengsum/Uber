39796f41aba57432ec833d4978eebcc1f126faa5
ANWDM <pv4003337@gmail.com>
2023-06-16 19:08:54 +0530

    Implementing otp screen

--- Screen/OTP.jsx
+++ Screen/OTP.jsx
@@ -1,21 +1,18 @@
-import { useState } from "react"
+import { useState ,useRef } from "react"
 import { SafeAreaView } from "react-native"
 import { TouchableOpacity ,View , Text ,TextInput , StyleSheet ,StatusBar} from "react-native"
 import { AntDesign } from '@expo/vector-icons';
 
+
+
 const OTP = () => {
-    const [otp , setOtp] = useState()
     return (
         <SafeAreaView style={styles.container}>
         <View className=" h-screen flex pt-10 pl-5 bg-slate-700">
           <AntDesign name="arrowleft" size={30} color="white" />
             <Text className="text-2xl font-bold mt-6 max-w-[320px] text-white">Enter the 4-digit code sent  to you at</Text>
             <Text className="text-white text-lg">84884804288.</Text>
-            <TextInput
-
-            />
             <Text>Resend code</Text>
-
             <TouchableOpacity>
                 <Text>Next</Text>
             </TouchableOpacity>
