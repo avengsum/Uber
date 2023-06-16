c2e2df72f955dca18e32d99188fcfd2f3c2b08e9
ANWDM <pv4003337@gmail.com>
2023-06-16 17:55:19 +0530

    started implemnting otp

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { Text, View } from 'react-native';
 import Login from './Screen/Login';
 import { SafeAreaView , StyleSheet} from 'react-native';
 import Choose from './Screen/Choose';
+import OTP from './Screen/OTP';
 
 export default function App() {
   return (
     <SafeAreaView >
     <View>
-      <Choose />
+      <OTP />
       <StatusBar style="auto" />
     </View>
     </SafeAreaView>
--- Screen/OTP.jsx
+++ Screen/OTP.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
-import { TouchableOpacity } from "react-native"
-import { TextInput } from "react-native"
+import { SafeAreaView } from "react-native"
+import { TouchableOpacity ,View , Text ,TextInput , StyleSheet ,StatusBar} from "react-native"
+import { AntDesign } from '@expo/vector-icons';
 
 const OTP = () => {
     const [otp , setOtp] = useState()
     return (
-        <View>
-            <Text>Enter the 4-digit code sent  to you at</Text>
+        <SafeAreaView style={styles.container}>
+        <View className=" h-screen flex pt-10 pl-5 bg-slate-700">
+          <AntDesign name="arrowleft" size={30} color="white" />
+            <Text className="text-2xl font-bold mt-6 max-w-[320px] text-white">Enter the 4-digit code sent  to you at</Text>
+            <Text className="text-white text-lg">84884804288.</Text>
             <TextInput
 
             />
@@ -16,7 +20,14 @@ const OTP = () => {
                 <Text>Next</Text>
             </TouchableOpacity>
         </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop:StatusBar.currentHeight
+    }
+  })
 
 export default OTP
