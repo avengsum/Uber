76e73a36a9bd19a43d42e92372ef9878fa08f800
ANWDM <pv4003337@gmail.com>
2023-06-16 17:35:59 +0530

    choose implemnted

--- Screen/Choose.jsx
+++ Screen/Choose.jsx
@@ -5,20 +5,31 @@ import { AntDesign } from '@expo/vector-icons';
 const Choose = () => {
     return (
         <SafeAreaView style={styles.container}>
-            <View className="h-screen flex bg-slate-700">
-            <AntDesign name="arrowleft" size={24} color="white" />
-                <Text className="text-2xl font-bold text-white" >Choose an account</Text>
-                <View>
-                <AntDesign name="facebook-square" size={24} color="black" />
-                    <Text>Facebook</Text>
-                <AntDesign name="google" size={24} color="black" />
-                    <Text>Google</Text>
-                </View>
-                <Text>By clicking a social option you may recieve an SMS for
-                    verification.Message and data rates may apply</Text>
-                <TouchableOpacity >
-                    <Text>Next</Text>
+            <View className="h-screen flex pt-10 pl-5 bg-slate-700">
+            <AntDesign name="arrowleft" size={30} color="white" />
+            <View className="mt-6"> 
+            <Text className="text-2xl font-bold text-white" >Choose an account</Text>
+            </View>
+                
+                <View className="flex gap-2">
+                <TouchableOpacity className="flex gap-5 flex-row">
+                <AntDesign name="facebook-square" size={24} color="white" />
+                    <Text className="text-white text-xl">Facebook</Text>
                 </TouchableOpacity>
+                <TouchableOpacity className="flex gap-5 flex-row">
+                <AntDesign name="google" size={24} color="white" />
+                    <Text className="text-white text-xl">Google</Text>
+                </TouchableOpacity>
+                
+                </View>
+                <Text className="text-white ml-2 mt-4 text-md max-w-[400px]">By clicking a social option you may recieve an SMS for
+                    verification.Message and data rates may apply</Text>
+                <View className="flex items-center mt-[400px] justify-center  ">
+                <TouchableOpacity className="bg-black px-14 py-5" >
+                    <Text className="text-white text-xl" >Next</Text>
+                </TouchableOpacity>
+                </View>
+                
             </View>
         </SafeAreaView>
     )
